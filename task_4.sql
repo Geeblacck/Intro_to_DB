8a2f31b431ca33cb5ef51a863b3c9f23b084c89f
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-10-19 11:06:22 +0100

    Initial commit

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- Use the correct database
 USE alx_book_store;
 
--- Show full details of the 'books' table without using DESCRIBE or EXPLAIN
+-- Display full description of the 'books' table without using DESCRIBE or EXPLAIN
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'Books'
