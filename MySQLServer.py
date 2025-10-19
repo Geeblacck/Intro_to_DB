fd92015315a21c30d469bca3c8059381c0fe002c
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-10-19 10:54:58 +0100

    Initial commit

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,6 @@ and creates a database named 'alx_book_store'.
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # Connect to MySQL Server
@@ -17,11 +16,11 @@ try:
 
     if connection.is_connected():
         cursor = connection.cursor()
-        # Create database (if not exists)
+        # Create database if not exists
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
